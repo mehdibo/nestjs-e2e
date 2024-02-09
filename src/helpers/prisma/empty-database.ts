7c60d74c844600ddffc09bf301e5d4c480f64f87
Mehdi Bounya <mehdi.bounya@gmail.com>
2024-02-09 17:43:56 +0100

    Create prisma helpers

---
+++ src/helpers/prisma/empty-database.ts
@@ -0,0 +1,22 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+export type DatabaseType = 'postgresql' | 'mysql';
+
+export async function emptyDatabase(prismaClient: PrismaClient, database: DatabaseType): Promise<void> {
+  let sql = '';
+  switch (database) {
+    case 'postgresql':
+      sql = 'TRUNCATE "$TABLE_NAME" RESTART IDENTITY CASCADE';
+      break;
+    case 'mysql':
+      sql = 'TRUNCATE TABLE "$TABLE_NAME"';
+  }
+
+  for (const key in Prisma.ModelName) {
+    const tableName = Prisma.ModelName[key];
+
+    await prismaClient.$queryRawUnsafe(
+      sql.replace('$TABLE_NAME', tableName),
+    );
+  }
+}
