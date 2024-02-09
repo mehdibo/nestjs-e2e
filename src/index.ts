d78b7f7a9c095f2d86c31b271ef7baffcdb4bbf5
Mehdi Bounya <mehdi.bounya@gmail.com>
2024-02-09 17:44:03 +0100

    Update index

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {createTestingModule, createApplication} from "./create-testing-module";
+
+export {loadFixtures} from "./helpers/prisma/fixtures-loader";
+export {emptyDatabase} from "./helpers/prisma/empty-database";
